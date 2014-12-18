edd63b7268e3e8e11b9e550a54886c40c43afea1
AnandSivaji <anandsivaj@gmail.com>
2014-12-19 00:17:11 +0530

    Cluster Concepts implementation

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-'use strict'
+'use strict';
+
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+
+    var cpuCount = require('os').cpus().length;
+
+    for (var i = 0; i < cpuCount; i += 1) { cluster.fork(); }
+
+    cluster.on('exit', function(worker) {
+
+        console.log('Worker ' + worker.id + ' is being died :(');
+        cluster.fork();
+    });
+
+} else {
+
+    var express = require('express');
+    var app = express();
+
+    app.get('/', function(request, response) {
+
+        response.send('Hello World from the worker: ' + cluster.worker.id)
+    });
+
+    app.listen(3000);
+    console.log('Worker ' + cluster.worker.id + ' is being started');
+}
